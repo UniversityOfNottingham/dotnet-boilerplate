d829ed128a8aab6fb26ab6c5dd97da9a6d3d87fb
Jonathan Couldridge <jcouldridge@outlook.com>
2018-03-08 10:01:59 +0000

    making comments family friendly

--- frontend/js/globals.js
+++ frontend/js/globals.js
@@ -3,7 +3,7 @@
 // This file contains anything that we (regrettably)
 // need to make available globally (i.e. on the `window` object)
 
-// jquery-validation-unobtrusive 3.2.6 is a twat and needs global jquery
+// jquery-validation-unobtrusive 3.2.6 needs global `jQuery`
 // 3.2.7 will fix this and the global exposition of jquery will no longer be needed
 import $ from 'jquery';
 window.jQuery = $;
