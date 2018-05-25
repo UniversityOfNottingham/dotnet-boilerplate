e3609b776bd473a98861c7044945e71ca1d7213d
Jonathan Couldridge <jcouldridge@outlook.com>
2018-05-25 10:49:22 +0100

    removed watch task (not sure about gulp4 for this one yet)

--- frontend/gulp-tasks/watch.js
+++
@@ -1,8 +0,0 @@
-﻿const gulp = require('gulp');
-const { src } = require('../package.json').paths;
-
-// Default and watch
-gulp.task('watch', ['default'], () => {
-  gulp.watch(`${src.js}/**/*.js`, ['js']);
-  gulp.watch(`${src.sass}/**/*.scss`, ['sass']);
-});
