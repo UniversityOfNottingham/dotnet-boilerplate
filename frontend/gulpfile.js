b99dd67098ee9ee7611933a713bbe44e7d016bd4
Jonathan Couldridge <jcouldridge@outlook.com>
2018-05-25 10:36:48 +0100

    empty gulpfile ready for 4, jquery globals no longer needed :tada:

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,5 @@
 // eslint-disable-next-line spaced-comment
 /// <binding BeforeBuild='default' />
 
-// Require each of our gulp tasks
-require('require-dir')('./gulp');
-
 // Default task
-require('gulp').task('default', ['clean', 'uglify', 'uglify-css']);
+// require('gulp').task('default', ['clean', 'uglify', 'uglify-css']);
