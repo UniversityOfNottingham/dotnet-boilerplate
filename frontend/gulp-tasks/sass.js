4f9d570a348a1fc9dc19384b8e15fb237166c979
Jonathan Couldridge <jcouldridge@outlook.com>
2018-05-25 10:59:00 +0100

    added sass gulp4 task and default task #5

--- frontend/gulp-tasks/sass.js
+++ frontend/gulp-tasks/sass.js
@@ -1,23 +1,15 @@
-﻿const { src, dest } = require('../package.json').paths;
+﻿const { src, dest } = require("../package.json").paths;
 
-const gulp = require('gulp');
-const header = require('gulp-header');
-const sass = require('gulp-sass');
-const notifier = require('node-notifier');
+const gulp = require("gulp");
+const header = require("gulp-header");
+const _sass = require("gulp-sass");
 
-const fileHeader = require('./file-header');
+const fileHeader = require("./file-header");
 
-// Compile SASS
-// Runs Lint task first
-gulp.task('sass', ['sass-lint'], () =>
-  gulp.src(`${src.sass}/**/*.scss`)
-    .pipe(sass({ outputStyle: 'expanded' }))
-    .on('error', function (err) {
-      sass.logError.call(this, err);
-      notifier.notify({
-        title: 'Gulp',
-        message: 'SASS error'
-      });
-    })
+module.exports = function sass() {
+  return gulp
+    .src(`${src.sass}/**/*.scss`)
+    .pipe(_sass({ outputStyle: "expanded" }))
     .pipe(header(fileHeader))
-    .pipe(gulp.dest(dest.root + dest.css)));
+    .pipe(gulp.dest(dest.root + dest.css));
+};
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -8,10 +8,15 @@ const clean = require("./gulp-tasks/clean");
 const eslint = require("./gulp-tasks/eslint");
 const js = require("./gulp-tasks/js");
 const sasslint = require("./gulp-tasks/sass-lint");
+const sass = require("./gulp-tasks/sass");
 
 // define compound tasks
 function buildJs(done) {
   return gulp.series(eslint, js)(done);
+}
+
+function buildSass(done) {
+  return gulp.series(sasslint, sass)(done);
 }
 
 // export everything that should be public
@@ -20,8 +25,10 @@ module.exports = {
   eslint,
   js,
   buildJs,
-  sasslint
+  sasslint,
+  sass,
+  buildSass
 };
 
 // define default task
-// require('gulp').task('default', ['clean', 'uglify', 'uglify-css']);
+gulp.task("default", gulp.series(clean, gulp.parallel(buildJs, buildSass)));
