bd35fdc86d44014d71b26cb3ef5152a67b06a4f2
Jonathan Couldridge <jcouldridge@outlook.com>
2018-05-25 10:48:21 +0100

    js bundle gulp4 task #5

--- frontend/gulp-tasks/js.js
+++ frontend/gulp-tasks/js.js
@@ -1,26 +1,27 @@
-const { dest, src } = require('../package.json').paths;
+const { dest, src } = require("../package.json").paths;
 
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
-const source = require('vinyl-source-stream');
-const rename = require('gulp-rename');
-const header = require('gulp-header');
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const browserify = require("browserify");
+const buffer = require("vinyl-buffer");
+const source = require("vinyl-source-stream");
+const rename = require("gulp-rename");
+const header = require("gulp-header");
 
-const fileHeader = require('./file-header');
+const fileHeader = require("./file-header");
 
-gulp.task('js', ['eslint'], () =>
-  browserify({
+module.exports = function js() {
+  return browserify({
     entries: `${src.js}/main.js`,
     debug: true
   })
-    .transform('babelify', { presets: [['env', { targets: { browsers: ['last 2 versions'] } }]] })
+    .transform("babelify")
     .bundle()
-    .pipe(source('main.js'))
+    .pipe(source("main.js"))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(header(fileHeader))
     .pipe(rename(`bundle.js`))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dest.root + dest.js)));
+    .pipe(gulp.dest(dest.root + dest.js));
+};
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -6,13 +6,19 @@ const gulp = require("gulp");
 // import individual tasks
 const clean = require("./gulp-tasks/clean");
 const eslint = require("./gulp-tasks/eslint");
+const js = require("./gulp-tasks/js");
 
 // define compound tasks
+function buildJs(done) {
+  return gulp.series(eslint, js)(done);
+}
 
 // export everything that should be public
 module.exports = {
   clean,
-  eslint
+  eslint,
+  js,
+  buildJs
 };
 
 // define default task
