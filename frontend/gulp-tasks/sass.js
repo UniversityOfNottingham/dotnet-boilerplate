4c799dee6068ce64d170dfd498fe6d0b615dd8cb
Alan Shortis <alan.shortis@mac.com>
2018-10-03 18:51:02 +0100

    Closes #11 - Add autoprefixer step (#12)

--- frontend/gulp-tasks/sass.js
+++ frontend/gulp-tasks/sass.js
@@ -2,6 +2,7 @@
 
 const gulp = require("gulp");
 const header = require("gulp-header");
+const autoprefixer = require('gulp-autoprefixer');
 const _sass = require("gulp-sass");
 
 const fileHeader = require("./file-header");
@@ -10,6 +11,7 @@ module.exports = function sass() {
   return gulp
     .src(`${src.sass}/**/*.scss`)
     .pipe(_sass({ outputStyle: "expanded" }))
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
     .pipe(header(fileHeader))
     .pipe(gulp.dest(dest.root + dest.css));
 };
